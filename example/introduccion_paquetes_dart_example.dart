fe7497d8fe4df0abc74f097a48cdd08b516686aa
jose09511 <jose09511@gmail.com>
2021-10-09 12:12:28 +0200

    first commit

---
+++ example/introduccion_paquetes_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:introduccion_paquetes_dart/introduccion_paquetes_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/introduccion_paquetes_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library introduccion_paquetes_dart;
+
+export 'src/introduccion_paquetes_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
